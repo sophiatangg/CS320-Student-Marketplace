19dd526978c2c2e5a7cb981d23035e53bb097318
Sophia Tang <sophiatang@umass.edu>
2024-10-31 12:48:27 -0400

    creating the connecting to database and add trade function

--- src/dbFuncs.js
+++ src/dbFuncs.js
@@ -1,23 +1,32 @@
-const mongoose = require('mongoose');
-const { Trade } = require('src/schemas.js');
-async function addTrade(itemId, proposedItemIds, buyerId, sellerId) {
-    try {
-      const expirationDate = new Date(Date.now() + 168 * 60 * 60 * 1000);
 
-      const newTrade = new Trade({
-        itemId,                    
-        proposedItemIds,           
-        buyerId,                  
-        sellerId,                  
-        timestamp: Date.now(),   
-        expiration: expirationDate, 
-        status: 'pending',        
-      });
-  
+const { Client } = require('pg')
 
-      const savedTrade = await newTrade.save();
-      console.log('Trade created:', savedTrade);
-    } catch (error) {
-      console.error('Error creating trade:', error);
+
+async function connectToDatabase() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5433,
+    database: 'student_market_place',
+    user: 'postgres',
+    password: 'shadow2004'
+  });
+
+  await client.connect(); // Connect to the database
+  return client; // Return the client instance for further use
+};
+
+async function addTrade(itemId, buyerId, sellerId, Initaltimestamp) {
+
+    try{
+
+      const initialTime = new Date(timestamp);
+      const expirationDate = new Date(initialTime.getTime() + 72 * 60 * 60 * 1000);
+      const query = `INSERT INTO trades (item_id, buyer_id, seller_id, timestamp, expiration_date) 
+      VALUES ($1, $2, $3, $4, $5)`;
+      const res = await client.query(query, [itemId, buyerId, sellerId, Initaltimestamp, expirationDate]);
+      console.log('trade added', res.rows[0].id)
+    }
+    finally {
+      await client.end();
     }
   }
