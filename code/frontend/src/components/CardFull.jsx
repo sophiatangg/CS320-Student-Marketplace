2cbe10489540b4e44694724b5a3dfd05005cb5c5
cmpdc <364850+cmpdc@users.noreply.github.com>
2024-11-15 13:59:14 -0500

    update

--- code/frontend/src/components/CardFull.jsx
+++ code/frontend/src/components/CardFull.jsx
@@ -64,7 +64,7 @@ const CardFull = (props) => {
 	const renderItemName = ({ text, highlight }) => {
 		if (!highlight) return text;
 
-		const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+		const parts = text?.split(new RegExp(`(${highlight})`, "gi"));
 		return parts.map((part, index) =>
 			part.toLowerCase() === highlight.toLowerCase() ? (
 				<span key={index} className={styles["highlight"]}>
